6b2ff9c16183a83aa2c5469233e391f92f64a259
TrinhDoan2203 <59088968+TrinhDoan2203@users.noreply.github.com>
2022-06-18 22:45:50 +0300

    Add files via upload

---
+++ MVVMExample/Model.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+namespace MVVMExample
+{
+    [Serializable]
+    public class Model : INotifyPropertyChanged
+    {
+        private string firstValue = "";
+        public string FirstValue 
+        { 
+            get { return firstValue; }
+            set
+            {
+                firstValue = value;
+                OnPropertyChange("FirstValue");
+            } 
+        }
+        private string secondValue = "";
+        public string SecondValue
+        {
+            get { return secondValue; }
+            set
+            {
+                secondValue = value;
+                OnPropertyChange("SecondValue");
+            }
+        }
+        private string result="";
+        public string Result
+        {
+            get { return result; }
+            set
+            {
+                result = value;
+                OnPropertyChange("Result");
+            }
+        }
+        private string arithmetics="";
+        public string Arithmetics
+        {
+            get { return arithmetics; }
+            set
+            {
+                arithmetics = value;
+                OnPropertyChange("Arithmetics");
+            }
+        }
+        public string FVHash { get; set; }
+        public string SVHash { get; set; }
+        public string ArithHash { get; set; }
+        public string ResultHash { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChange(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
